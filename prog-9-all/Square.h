ec254e5d7b6806df60336bf604ddc4d997350c51
Kaloian Doganov <doganov@projectoria.bg>
2011-12-12 18:41:24 +0200

    Example program 9.4.

---
+++ prog-9-all/Square.h
@@ -0,0 +1,7 @@
+#import "Rectangle.h"
+
+@interface Square: Rectangle
+
+-(void) setSide: (int) s;
+-(int) side;
+@end
---
+++ prog-9-all/prog-9-3.m
@@ -0,0 +1,59 @@
+// Illustrate Dynamic Typing and Binding
+
+#import "Square.h"
+
+int main (int argc, char *argv[])
+{
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+  Square *mySquare = [[Square alloc] init];
+
+  // isMemberOf:
+
+  if ( [mySquare isMemberOfClass: [Square class]] == YES )
+    NSLog (@"mySquare is a member of Square class");
+
+  if ( [mySquare isMemberOfClass: [Rectangle class]] == YES)
+    NSLog (@"mySquare is a member of Rectangle class");
+
+  if ( [mySquare isMemberOfClass: [NSObject class]] == YES)
+    NSLog (@"mySquare is a member of NSObject class");
+
+  // isKindOf
+
+  if ( [mySquare isKindOfClass: [Square class]] == YES)
+    NSLog (@"mySquare is a kind of Square");
+
+  if ( [mySquare isKindOfClass: [Rectangle class]] == YES )
+    NSLog (@"mySquare is a kind of Rectangle");
+
+  if ( [mySquare isKindOfClass: [NSObject class]] == YES )
+    NSLog (@"mySquare is a kind of NSObject");
+
+  // respondsTo:
+
+  if ( [mySquare respondsToSelector: @selector (setSide:)] == YES )
+    NSLog (@"mySquare responds to setSide: method");
+
+  if ( [mySquare respondsToSelector: @selector (setWidth:andHeight:)] == YES )
+    NSLog (@"mySquare responds to setWidth:andHeight: method");
+
+  if ( [Square respondsToSelector: @selector (alloc)] == YES )
+      NSLog (@"Square class responds to alloc method");
+
+  // instancesRespondTo:
+
+  if ([Rectangle instancesRespondToSelector: @selector (setSide:)] == YES)
+    NSLog (@"Instances of Rectangle respond to setSide: method");
+
+  if ([Square instancesRespondToSelector: @selector (setSide:)] == YES)
+    NSLog (@"Instances of Square respond to setSide: method");
+
+  if ([Square isSubclassOfClass: [Rectangle class]] == YES)
+    NSLog (@"Square is a subclass of a rectangle");
+
+  [mySquare release];
+
+  [pool drain];
+  return 0;
+}
