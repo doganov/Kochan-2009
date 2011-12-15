0206d496bd23680bf8bef15148d65138f7469e75
Kaloian Doganov <doganov@projectoria.bg>
2011-12-15 12:37:31 +0200

    Exercise 11.1

---
+++ ex-11-fract/Fraction.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+// Define the Fraction class
+
+@interface Fraction : NSObject
+{
+  int numerator;
+  int denominator;
+}
+
+@property int numerator, denominator;
+
+-(void) print;
+-(void) setTo: (int) n over: (int) d;
+-(double) convertToNum;
+-(void) reduce;
+
+@end
---
+++ ex-11-fract/ex-11-1.m
@@ -0,0 +1,127 @@
+#import "Fraction.h"
+
+@interface Fraction (MathOps)
+-(Fraction *) add: (Fraction *) f;
+-(Fraction *) mul: (Fraction *) f;
+-(Fraction *) sub: (Fraction *) f;
+-(Fraction *) div: (Fraction *) f;
+-(Fraction *) invert;
+@end
+
+@implementation Fraction (MathOps)
+
+-(Fraction *) add: (Fraction *) f
+{
+  // To add two fractions:
+  // a/b + c/d = ((a*d) + (b*c)) / (b * d)
+
+  // result will store the result of the addition
+  Fraction *result = [[Fraction alloc] init];
+  int resultNum, resultDenom;
+
+  resultNum = numerator * f.denominator + denominator * f.numerator;
+  resultDenom = denominator * f.denominator;
+
+  [result setTo: resultNum over: resultDenom];
+  [result reduce];
+
+  return result;
+}
+
+-(Fraction *) sub: (Fraction *) f
+{
+  // To sub two fractions:
+  // a/b - c/d = ((a*d) - (b*c)) / (b * d)
+
+  Fraction *result = [[Fraction alloc] init];
+  int resultNum, resultDenom;
+
+  resultNum = (numerator * f.denominator) - (denominator * f.numerator);
+  resultDenom = denominator * f.denominator;
+
+  [result setTo: resultNum over: resultDenom];
+  [result reduce];
+
+  return result;
+}
+
+-(Fraction *) mul: (Fraction *) f
+{
+  Fraction *result = [[Fraction alloc] init];
+
+  [result setTo: numerator * f.numerator
+           over: denominator * f.denominator];
+  [result reduce];
+
+  return result;
+}
+
+-(Fraction *) div: (Fraction *) f
+{
+  Fraction *result = [[Fraction alloc] init];
+
+  [result setTo: numerator * f.denominator
+           over: denominator * f.numerator];
+  [result reduce];
+
+  return result;
+}
+
+-(Fraction *) invert
+{
+  Fraction *result = [[Fraction alloc] init];
+
+  [result setTo: denominator over: numerator];
+  [result reduce];
+
+  return result;
+}
+@end
+
+int main (int argc, char *argv[])
+{
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+  Fraction *a = [[Fraction alloc] init];
+  Fraction *b = [[Fraction alloc] init];
+  Fraction *result;
+
+  [a setTo: 1 over: 3];
+  [b setTo: 2 over: 5];
+
+  [a print]; NSLog (@"  +"); [b print]; NSLog (@"-----");
+  result = [a add: b];
+  [result print];
+  NSLog (@"\n");
+  [result release];
+
+  [a print]; NSLog (@"  -"); [b print]; NSLog (@"-----");
+  result = [a sub: b];
+  [result print];
+  NSLog (@"\n");
+  [result release];
+
+  [a print]; NSLog (@"  *"); [b print]; NSLog (@"-----");
+  result = [a mul: b];
+  [result print];
+  NSLog (@"\n");
+  [result release];
+
+  [a print]; NSLog (@"  /"); [b print]; NSLog (@"-----");
+  result = [a div: b];
+  [result print];
+  NSLog (@"\n");
+  [result release];
+
+  NSLog (@"invert"); [a print]; NSLog (@"-----");
+  result = [a invert];
+  [result print];
+  NSLog (@"\n");
+  [result release];
+  
+  [a release];
+  [b release];
+
+  [pool drain];
+  return 0;
+}
