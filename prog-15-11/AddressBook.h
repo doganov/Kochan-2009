e524aca9dcb1e67ff50aedb859609cd118271b36
Kaloian Doganov <doganov@projectoria.bg>
2011-12-20 17:23:33 +0200

    Example program 15.11.

---
+++ prog-15-11/AddressBook.h
@@ -0,0 +1,17 @@
+#import <Foundation/NSArray.h>
+#import "AddressCard.h"
+
+@interface AddressBook: NSObject
+{
+  NSString *bookName;
+  NSMutableArray *book;
+}
+
+-(id) initWithName: (NSString *) name;
+-(void) addCard: (AddressCard *) theCard;
+-(int) entries;
+-(void) list;
+-(AddressCard *) lookup: (NSString *) theName;
+-(void) dealloc;
+
+@end
