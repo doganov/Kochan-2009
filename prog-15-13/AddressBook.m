c73e87d63eb0579d05cac9212b0449fb2336899c
Kaloian Doganov <doganov@projectoria.bg>
2011-12-20 17:36:26 +0200

    Example program 15.13.

---
+++ prog-15-13/AddressBook.m
@@ -0,0 +1,66 @@
+#import "AddressBook.h"
+
+@implementation AddressBook;
+
+// set up the AddressBook's name and an empty book
+
+-(id) initWithName: (NSString *) name
+{
+  self = [super init];
+
+  if (self) {
+    bookName = [[NSString alloc] initWithString: name];
+    book = [[NSMutableArray alloc] init];    
+  }
+
+  return self;
+}
+
+-(void) addCard: (AddressCard *) theCard
+{
+  [book addObject: theCard];
+}
+
+-(void) removeCard: (AddressCard *) theCard
+{
+  [book removeObjectIdenticalTo: theCard];
+}
+
+-(int) entries
+{
+  return [book count];
+}
+
+-(void) list
+{
+  NSLog (@"======== Contents of: %@ =========", bookName);
+  for ( AddressCard *theCard in book )
+    NSLog (@"%-20s    %-32s",
+	   [theCard.name UTF8String],
+	   [theCard.email UTF8String]);
+
+  NSLog (@"====================================================");
+}
+
+-(AddressCard *) lookup: (NSString *) theName
+{
+  for ( AddressCard *nextCard in book )
+    if ( [[nextCard name] caseInsensitiveCompare: theName] == NSOrderedSame )
+      return nextCard;
+
+  return nil;
+}
+
+-(void) sort
+{
+  [book sortUsingSelector: @selector(compareNames:)];
+}
+
+-(void) dealloc
+{
+  [bookName release];
+  [book release];
+  [super dealloc];
+}
+
+@end
