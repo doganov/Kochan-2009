6dbce8bf0b89bd121ca1fab1e7f7658006f70ff7
Kaloian Doganov <kaloian@doganov.org>
2012-01-07 16:08:40 +0200

    Exercise 16.6.

---
+++ ex-16-6.m
@@ -0,0 +1,63 @@
+#import <Foundation/NSObject.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSFileHandle.h>
+#import <Foundation/NSFileManager.h>
+
+#import <Foundation/NSAutoreleasePool.h>
+#import <Foundation/NSData.h>
+
+#define kBufSize 8192
+
+int main(int argc, char *argv[])
+{
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  NSFileHandle *inFile, *outFile;
+  NSData *buffer;
+
+  // Open the file testfile for reading
+
+  inFile = [NSFileHandle fileHandleForReadingAtPath: @"testfile"];
+
+  if (inFile == nil) {
+    NSLog (@"Open of testfile for reading failed");
+    return 1;
+  }
+
+  // Create the output file first if necessary
+
+  [[NSFileManager defaultManager] createFileAtPath: @"testout"
+                                          contents: nil
+                                        attributes: nil];
+
+  // Now open outfile for writing
+
+  outFile = [NSFileHandle fileHandleForWritingAtPath: @"testout"];
+
+  if (outFile == nil) {
+    NSLog (@"Open of testout for writing failed");
+    return 2;    
+  }
+
+  // Truncate the output file since it may contain data
+
+  [outFile truncateFileAtOffset: 0];
+
+  // Read the data from inFile and write it to outFile
+
+  while ([(buffer = [inFile readDataOfLength: kBufSize]) length] > 0)
+    [outFile writeData: buffer];
+
+  // Close the two files
+
+  [inFile closeFile];
+  [outFile closeFile];
+
+  // Verify the file's contents
+
+  // NSLog (@"%@", [NSString stringWithContentsOfFile: @"testout"
+  //                                         encoding: NSUTF8StringEncoding
+  //                                            error: NULL]);
+
+  [pool drain];
+  return 0;
+}
